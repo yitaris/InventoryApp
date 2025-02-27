43d3f823274b5b466c10d8f3077d8f5c9511c5bc
Yiğit Bayır <yitaris>
2025-02-28 02:07:09 +0300

    kapattım

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+//import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <div>
+      Site Kullanıma Kapalıdır iletişim: Yitariss@gmail.com
+    </div>
   </StrictMode>
 )
