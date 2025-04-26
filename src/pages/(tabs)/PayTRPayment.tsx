873206ee17900281cb54b6177068723957738183
Yiğit Bayır <yitaris>
2025-04-26 20:06:23 +0300

    payments

--- src/pages/(tabs)/PayTRPayment.tsx
+++ src/pages/(tabs)/PayTRPayment.tsx
@@ -2,7 +2,7 @@ const PayTRPayment: React.FC = () => {
   
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-black fixed top-0 left-0">
-      Payment Page
+      Payment Pages
     </div>
   );
 };
